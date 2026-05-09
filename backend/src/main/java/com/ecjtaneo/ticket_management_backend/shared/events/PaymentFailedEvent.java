3d3881a5941736e7c1af3b04839bfab52f91ccb4
EcjTn <ecjtaneo@gmail.com>
2026-05-09 16:34:35 +0800

    feat(backend): add payment events in advance

---
+++ backend/src/main/java/com/ecjtaneo/ticket_management_backend/shared/events/PaymentFailedEvent.java
@@ -0,0 +1,5 @@
+package com.ecjtaneo.ticket_management_backend.shared.events;
+
+public record PaymentFailedEvent(Long orderId) {
+
+}
---
+++ backend/src/main/java/com/ecjtaneo/ticket_management_backend/shared/events/PaymentSuccessfulEvent.java
@@ -0,0 +1,5 @@
+package com.ecjtaneo.ticket_management_backend.shared.events;
+
+public record PaymentSuccessfulEvent(Long orderId) {
+
+}

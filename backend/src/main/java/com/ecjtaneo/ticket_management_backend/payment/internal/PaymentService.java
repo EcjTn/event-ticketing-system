06edb36f4c8e05c7f8059f6f93f9a0edfe66f108
EcjTn <ecjtaneo@gmail.com>
2026-05-09 16:44:42 +0800

    feat(backend): add payment mock processing

---
+++ backend/src/main/java/com/ecjtaneo/ticket_management_backend/payment/internal/PaymentService.java
@@ -0,0 +1,32 @@
+package com.ecjtaneo.ticket_management_backend.payment.internal;
+
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.modulith.events.ApplicationModuleListener;
+import org.springframework.stereotype.Service;
+
+import com.ecjtaneo.ticket_management_backend.shared.events.OrderCreatedEvent;
+import com.ecjtaneo.ticket_management_backend.shared.events.PaymentFailedEvent;
+import com.ecjtaneo.ticket_management_backend.shared.events.PaymentSuccessfulEvent;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+    private final ApplicationEventPublisher eventPublisher;
+
+    @ApplicationModuleListener()
+    public void processOrderCreation(OrderCreatedEvent event) {
+
+        // Just for demo: simulate a random payment success/failure
+        boolean shouldFail = true;
+
+        if (shouldFail) {
+            eventPublisher.publishEvent(new PaymentFailedEvent(event.orderId()));
+        } else {
+            eventPublisher.publishEvent(new PaymentSuccessfulEvent(event.orderId()));
+        }
+
+    }
+
+}

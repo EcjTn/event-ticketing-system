55c19d921622e65f0a69435285a9c2d359041708
EcjTn <ecjtaneo@gmail.com>
2026-05-19 15:12:30 +0800

    refactor(backend): add event-basic dto more info

--- backend/src/main/java/com/ecjtaneo/ticket_management_backend/event/internal/dto/EventBasicInfoResponseDto.java
+++ backend/src/main/java/com/ecjtaneo/ticket_management_backend/event/internal/dto/EventBasicInfoResponseDto.java
@@ -3,7 +3,9 @@ package com.ecjtaneo.ticket_management_backend.event.internal.dto;
 public record EventBasicInfoResponseDto(
     Long id,
     String name,
-    String imageUrl
+    String imageUrl,
+    String venue,
+    String description
 ) {
     
 }

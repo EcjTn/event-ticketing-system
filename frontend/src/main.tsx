4485993f333a61bd53e835a8724b5201d255954d
EcjTn <ecjtaneo@gmail.com>
2026-05-20 10:26:12 +0800

    refactor(frontend): Change fonts

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import '@fontsource/geist-sans/latin.css'
+import '@fontsource/geist-mono/latin.css'
 import './index.css'
 import App from './App.tsx'
 

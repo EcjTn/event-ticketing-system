ed48a93271e3f9cd8ab8f3997a198fefba933eb2
EcjTn <ecjtaneo@gmail.com>
2026-05-20 16:09:50 +0800

    fix(backend): add missing Bean stripe client

--- backend/src/main/java/com/ecjtaneo/ticket_management_backend/payment/internal/StripeConfig.java
+++ backend/src/main/java/com/ecjtaneo/ticket_management_backend/payment/internal/StripeConfig.java
@@ -1,12 +1,14 @@
 package com.ecjtaneo.ticket_management_backend.payment.internal;
 
 import com.stripe.StripeClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StripeConfig {
 
     //TODO: Load API key from configuration file
+    @Bean
     public StripeClient stripeClient() {
         return new StripeClient("API_KEY");
     }

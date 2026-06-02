d3fdf94aa15e44d2b1ad90f5543dc35b762d52c7
EcjTn <ecjtaneo@gmail.com>
2026-06-02 13:26:00 +0800

    feat(backend): add ticket validation controller

--- backend/src/main/java/com/ecjtaneo/ticket_management_backend/ticket/internal/TicketController.java
+++ backend/src/main/java/com/ecjtaneo/ticket_management_backend/ticket/internal/TicketController.java
@@ -1,7 +1,11 @@
 package com.ecjtaneo.ticket_management_backend.ticket.internal;
 
 import com.ecjtaneo.ticket_management_backend.shared.dtos.MessageResponse;
+import com.ecjtaneo.ticket_management_backend.ticket.internal.dto.TicketValidationRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class TicketController {
     private final TicketService service;
 
-//    @
-//    MessageResponse validateAndUseTicket(String uniqueCode, Long eventId) {
-//        return service.validateAndUseTicket(uniqueCode, eventId);
-//    }
+    @PostMapping("/validate")
+    MessageResponse validateTicket(@RequestBody @Valid TicketValidationRequest dto) {
+        return service.validateAndUseTicket(dto);
+    }
 
 }

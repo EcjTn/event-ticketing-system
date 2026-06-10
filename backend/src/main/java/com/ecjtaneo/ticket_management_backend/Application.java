5359efe6dd2ab7abc0a2f9a7f590c10f749c0638
EcjTn <ecjtaneo@gmail.com>
2026-06-10 09:42:23 +0800

    feat(backend): add redis configuration properties

--- backend/src/main/java/com/ecjtaneo/ticket_management_backend/Application.java
+++ backend/src/main/java/com/ecjtaneo/ticket_management_backend/Application.java
@@ -3,7 +3,7 @@ package com.ecjtaneo.ticket_management_backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+//TODO: Add EnableCaching and Redis Cache Configuration.
 @SpringBootApplication
 @EnableScheduling
 public class Application {

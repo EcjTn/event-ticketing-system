bc39fbedf3febaa7724b4cbfe5409f85301e5dd6
EcjTn <ecjtaneo@gmail.com>
2026-06-12 18:27:50 +0800

    feat(backend): add enable caching annotation

--- backend/src/main/java/com/ecjtaneo/ticket_management_backend/Application.java
+++ backend/src/main/java/com/ecjtaneo/ticket_management_backend/Application.java
@@ -2,10 +2,12 @@ package com.ecjtaneo.ticket_management_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
-//TODO: Add EnableCaching and Redis Cache Configuration.
+//TODO: Add Redis Cache Configuration.
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class Application {
 
 	public static void main(String[] args) {

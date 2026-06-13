768e78c14b577a1fbe190a97c1aeffa89131707e
EcjTn <ecjtaneo@gmail.com>
2026-06-13 18:47:10 +0800

    feat(backend): add redis cache configuration

---
+++ backend/src/main/java/com/ecjtaneo/ticket_management_backend/infrastructure/config/RedisConfig.java
@@ -0,0 +1,28 @@
+package com.ecjtaneo.ticket_management_backend.infrastructure.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+import java.time.Duration;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(5))
+                .disableCachingNullValues();
+
+        return RedisCacheManager
+                .builder(redisConnectionFactory)
+                .cacheDefaults(config)
+                .build();
+    }
+
+}
